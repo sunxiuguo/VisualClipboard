8ebbaf820b117e8af60d2f4892d3280075192b80
Sun Xiu Guo <sunxg@fenbi.com>
2019-05-20 22:33:37 +0800

    FIX: corejs warnning

--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -7,41 +7,42 @@ import webpack from 'webpack';
 import { dependencies } from '../package.json';
 
 export default {
-  externals: [...Object.keys(dependencies || {})],
+    externals: [...Object.keys(dependencies || {})],
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true
-          }
-        }
-      }
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true
+                    }
+                }
+            }
+        ]
+    },
+
+    output: {
+        path: path.join(__dirname, '..', 'app'),
+        // https://github.com/webpack/webpack/issues/1114
+        libraryTarget: 'commonjs2'
+    },
+
+    /**
+     * Determine the array of extensions that should be used to resolve modules.
+     */
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: { 'react-dom': '@hot-loader/react-dom' }
+    },
+
+    plugins: [
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: 'production'
+        }),
+
+        new webpack.NamedModulesPlugin()
     ]
-  },
-
-  output: {
-    path: path.join(__dirname, '..', 'app'),
-    // https://github.com/webpack/webpack/issues/1114
-    libraryTarget: 'commonjs2'
-  },
-
-  /**
-   * Determine the array of extensions that should be used to resolve modules.
-   */
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'production'
-    }),
-
-    new webpack.NamedModulesPlugin()
-  ]
 };
