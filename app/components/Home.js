d7b14d0a7df59890d169f53ba4b3bb1d8a6d69bb
Sun Xiu Guo <sunxg@fenbi.com>
2019-05-21 10:14:58 +0800

    ADD: Material UI

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,24 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
 import routes from '../constants/routes';
 import styles from './Home.css';
 
 type Props = {};
 
 export default class Home extends Component<Props> {
-  props: Props;
+    props: Props;
 
-  render() {
-    return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={styles.container} data-tid="container">
+                <Button variant="contained" color="primary">
+                    按钮1
+                </Button>
+                <Link to={routes.COUNTER}>to Counter</Link>
+            </div>
+        );
+    }
 }
