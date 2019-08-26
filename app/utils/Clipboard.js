01b3025245806d752132ac500701d9bcbf0d1f9b
Sun Xiu Guo <sunxg@fenbi.com>
2019-08-26 15:50:41 +0800

    FIX: 使用node-schedule 代替setInterval

--- app/utils/Clipboard.js
+++ app/utils/Clipboard.js
@@ -1,6 +1,7 @@
 import DataBase from './IndexedDB';
 
 const { clipboard } = require('electron');
+const schedule = require('node-schedule');
 
 const Db = new DataBase();
 
@@ -13,46 +14,43 @@ export default class Clipboard {
 
     startWatching = () => {
         if (!this.watcherId) {
-            // todo 使用node-schedule来做定时任务
-            this.watcherId = setInterval(() => {
-                if (
-                    Clipboard.isDiffText(
-                        this.previousText,
-                        clipboard.readText()
-                    )
-                ) {
-                    this.previousText = clipboard.readText();
-                    Db.add('text', {
-                        createTime: Date.now(),
-                        content: this.previousText
-                    });
-                }
-                if (
-                    Clipboard.isDiffImage(
-                        this.previousImage,
-                        clipboard.readImage()
-                    )
-                ) {
-                    this.previousImage = clipboard.readImage();
-                    if (this.previousImage) {
-                        Db.add('image', {
-                            createTime: Date.now(),
-                            content: this.previousImage.toDataURL()
-                        });
-                    }
-                }
-            }, 500);
+            this.watcherId = schedule.scheduleJob('* * * * * *', () => {
+                Clipboard.writeText();
+                Clipboard.writeImage();
+            });
         }
         return clipboard;
     };
 
     stopWatching = () => {
         if (this.watcherId) {
-            clearInterval(this.watcherId);
+            this.watcherId.cancel();
         }
         this.watcherId = null;
         return clipboard;
     };
+
+    static writeText() {
+        if (Clipboard.isDiffText(this.previousText, clipboard.readText())) {
+            this.previousText = clipboard.readText();
+            Db.add('text', {
+                createTime: Date.now(),
+                content: this.previousText
+            });
+        }
+    }
+
+    static writeImage() {
+        if (Clipboard.isDiffImage(this.previousImage, clipboard.readImage())) {
+            this.previousImage = clipboard.readImage();
+            if (this.previousImage) {
+                Db.add('image', {
+                    createTime: Date.now(),
+                    content: this.previousImage.toDataURL()
+                });
+            }
+        }
+    }
 
     static isDiffText(str1, str2) {
         return str2 && str1 !== str2;
