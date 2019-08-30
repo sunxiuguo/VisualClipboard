a86ac25923e91dbcf0dc39fe8f9c856d492afc5d
Sun Xiu Guo <sunxg@fenbi.com>
2019-08-30 18:44:39 +0800

    MOD: use fs .png instead of base64

--- app/utils/Clipboard.js
+++ app/utils/Clipboard.js
@@ -2,14 +2,25 @@ import DataBase from './IndexedDB';
 
 const { clipboard } = require('electron');
 const schedule = require('node-schedule');
+const md5 = require('md5');
+const fs = require('fs');
 
 const Db = new DataBase();
+const hostPath = `${__dirname}/temp`;
+
+function xMkdirSync(dirname) {
+    if (!fs.existsSync(dirname)) {
+        fs.mkdirSync(dirname);
+    }
+}
+
+xMkdirSync(hostPath);
 
 export default class Clipboard {
     constructor() {
         this.watcherId = null;
         this.previousText = clipboard.readText();
-        this.previousImage = clipboard.readImage();
+        this.previousImageMd5 = md5(clipboard.readImage().toPNG());
     }
 
     startWatching = () => {
@@ -41,14 +52,23 @@ export default class Clipboard {
     }
 
     static writeImage() {
-        const clipboardImage = clipboard.readImage();
-        if (Clipboard.isDiffImage(this.previousImage, clipboardImage)) {
-            this.previousImage = clipboardImage;
-            if (this.previousImage) {
-                Db.add('image', {
-                    createTime: Date.now(),
-                    content: this.previousImage.toDataURL(),
-                    ratio: this.previousImage.getAspectRatio() // 图片长宽比
+        const nativeImage = clipboard.readImage();
+        const pngBuffer = nativeImage.toPNG();
+        const md5String = md5(pngBuffer);
+
+        if (Clipboard.isDiffText(this.previousImageMd5, md5String)) {
+            this.previousImageMd5 = md5String;
+            if (!nativeImage.isEmpty()) {
+                const path = `${hostPath}/${md5String}.png`;
+                fs.writeFile(path, pngBuffer, err => {
+                    if (!err) {
+                        Db.add('image', {
+                            createTime: Date.now(),
+                            content: path
+                        });
+                    } else {
+                        console.error(err);
+                    }
                 });
             }
         }
@@ -56,15 +76,5 @@ export default class Clipboard {
 
     static isDiffText(str1, str2) {
         return str2 && str1 !== str2;
-    }
-
-    static isDiffImage(preImage, currentImage) {
-        if (!preImage) {
-            return true;
-        }
-        return (
-            !currentImage.isEmpty() &&
-            preImage.toDataURL() !== currentImage.toDataURL()
-        );
     }
 }
